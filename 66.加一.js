17d08bf12333252a56cec95f7fce64b5adda8fa0
ydc <395670149@qq.com>
2020-12-16 13:52:24 +0800

    second try

--- 66.加一.js
+++ 66.加一.js
@@ -10,7 +10,15 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-
+  digits[digits.length-1]++
+  for(var i = digits.length-1 ; i >= 0 ; i--){
+    if(digits[i]==10){
+      digits[i]=0
+      if(i==0) digits.unshift(1)
+      else digits[i-1]++
+    }
+  }
+  return digits
 };
 // @lc code=end
 
