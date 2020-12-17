dbbedcb778e79502b3ff120096d9485a11fd75fd
ydc <395670149@qq.com>
2020-12-17 10:24:04 +0800

    204 unsuccess

---
+++ 204.计数质数.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=204 lang=javascript
+ *
+ * [204] 计数质数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+//   if(n==0) return 0
+//   if(n==1) return 0
+//   if(n==2) return 0
+//   if(n==3) return 1 
+//   var temp = 0
+//   for(var i = 2 ; i < n ; i++){
+//     for(var j = 2 ; j < i ; j++){
+//     if(i%j==0) {temp++;break;}
+//     }
+//   }
+//   return n-2-temp
+// };
+// Time Limit Exceeded
+// @lc code=end
+
